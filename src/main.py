80718d3b0a1f49aac53ab55ab43eea4cbabf68b1
Giovane Miranda <credeal1360>
2021-07-15 13:12:37 -0300

    Profit - season barchart

--- src/main.py
+++ src/main.py
@@ -12,8 +12,8 @@ def extraction_dataset(path):
 
 def apply_date_seasonality(month):
     return 'Winter' if (month == 1) | (month == 2) | (month == 12) \
-         else ('Spring' if (month >= 3) & (month <= 5)
-               else ('Summer' if (month >= 6) & (month <= 8) else 'Fall'))
+        else ('Spring' if (month >= 3) & (month <= 5)
+              else ('Summer' if (month >= 6) & (month <= 8) else 'Fall'))
 
 
 def convert_sqft_m2(foot):
@@ -21,17 +21,20 @@ def convert_sqft_m2(foot):
 
 
 def calculate_percentage(value, percentage):
-    return value * (percentage/100)
+    return value * (percentage / 100)
 
 
 def transform_date(data):
     """Generates a column seasonality based on date"""
+
     data['date'] = pd.to_datetime(data['date']).dt.date
+
     return data
 
 
 def gen_buying_table(df_house):
     """Generates a columns with buying suggestion"""
+
     df_purchase = df_house.copy()
     df_purchase['median_price_zip'] = df_purchase.groupby('zipcode')['price'].transform('median')
 
@@ -49,12 +52,13 @@ def gen_buying_table(df_house):
 
 def gen_sale_table(df_house):
     """Generate df of sale analysis, calculating the median price per zipcode and seasonality"""
+
     df_sale = df_house[['id', 'zipcode', 'price', 'date']].copy()
 
     # creating new column seasonality, describing the season the house went on sale
     df_sale['seasonality'] = pd.to_datetime(df_sale['date']).dt.month.apply(apply_date_seasonality)
 
-    #generate df of median price per zipcode and seasonality
+    # generate df of median price per zipcode and seasonality
     df_median_seasonality = df_sale.groupby(['zipcode', 'seasonality'])['price'].median().to_frame(
         name='median_price_seasonality').reset_index()
 
@@ -62,11 +66,14 @@ def gen_sale_table(df_house):
 
     for index, row in df_house_merge_median.iterrows():
         if row['price'] > row['median_price_seasonality']:
-            df_house_merge_median.loc[index, 'selling_price_suggestion'] = row['price'] + calculate_percentage(row['price'], 10)
+            df_house_merge_median.loc[index, 'selling_price_suggestion'] = row['price'] + calculate_percentage(
+                row['price'], 10)
         else:
-            df_house_merge_median.loc[index, 'selling_price_suggestion'] = row['price'] + calculate_percentage(row['price'], 30)
+            df_house_merge_median.loc[index, 'selling_price_suggestion'] = row['price'] + calculate_percentage(
+                row['price'], 30)
 
-    df_house_merge_median['expected_profit'] = df_house_merge_median['selling_price_suggestion'] - df_house_merge_median['price']
+    df_house_merge_median['expected_profit'] = df_house_merge_median['selling_price_suggestion'] - \
+                                               df_house_merge_median['price']
 
     df_house_merge_median.to_csv('../data/processed/kc_house_sale.csv', index=False)
 
@@ -74,13 +81,16 @@ def gen_sale_table(df_house):
 
 
 def gen_profit_table(data_purchase, data_sale):
-    df_purchase_filtered = data_purchase.query('buying_analysis == "Buy"')
+    """Generate profit table, based on each house and or price per season"""
+
+    df_purchase_filtered = data_purchase.query('buying_analysis == "Buy"').copy()
     data_merge = df_purchase_filtered.merge(data_sale[['id', 'seasonality', 'median_price_seasonality',
-                                                              'selling_price_suggestion', 'expected_profit']], on='id')
+                                                       'selling_price_suggestion', 'expected_profit']], on='id')
 
     l_house = list(data_merge['id'].unique())
     l_profit = []
 
+    # iterate each house price per season, taking the one with max profit
     for id_house in l_house:
         row = data_merge[data_merge['id'] == id_house]['expected_profit'].idxmax()
         l_profit.append(data_merge.loc[row].to_dict())
--- src/page.py
+++ src/page.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import pandas as pd
+import plotly.express as px
 
 
 def highlight_suggestion(val):
@@ -7,11 +8,11 @@ def highlight_suggestion(val):
     return 'background-color: %s' % color
 
 
-def purchase_analysis_ui(df_purchase, f_column_buying, f_only_buy):
+def purchase_analysis_ui(df_purchase, f_column_buying, f_buying_price, f_only_buy):
     if f_only_buy:
         df_purchase = df_purchase.query('buying_analysis == "Buy"')
 
-    df_purchase_filtered = df_purchase.loc[:, f_column_buying].copy()
+    df_purchase_filtered = df_purchase.loc[df_purchase['price'] <= f_buying_price, f_column_buying].copy()
     df_purchase_style = df_purchase_filtered.style.applymap(highlight_suggestion,
                                                             subset=pd.IndexSlice[:, ['buying_analysis']])
     st.dataframe(df_purchase_style)
@@ -19,44 +20,57 @@ def purchase_analysis_ui(df_purchase, f_column_buying, f_only_buy):
     return None
 
 
-def sale_analysis_ui(df_sale, filter_column_sale ):
-    df_sale_filtered = df_sale.loc[:, filter_column_sale]
+def sale_analysis_ui(df_sale, f_column_sale):
+
+    df_sale_filtered = df_sale.loc[:, f_column_sale]
     st.dataframe(df_sale_filtered)
+    df_season_profit = df_sale_filtered[['seasonality', 'expected_profit']].groupby('seasonality').sum()
 
+    fig_season_profit = px.bar(df_season_profit)
+    st.plotly_chart(fig_season_profit, use_container_width=True)
     return None
-
 
 
 def run_ui(df_house_purchase, df_house_sale, df_house_profit):
     """Generate main page"""
 
-    st.sidebar.title('Filtros House Rocket ')
+    # creating sidebar
+    st.sidebar.title('House Rocket Filter')
     st.sidebar.subheader('Buying Suggestions')
 
     # filter : Properties Attributes
     f_column_purchase = st.sidebar.multiselect('Properties Attributes', options=list(df_house_purchase.columns),
                                                default=['id', 'zipcode', 'price', 'condition',
                                                         'median_price_zip', 'buying_analysis'])
+    # filter: house max price
+    f_buying_price = st.sidebar.slider('Select maximum price', min_value=int(df_house_purchase['price'].min()),
+                                       max_value=int(df_house_purchase['price'].max()),
+                                       value=int(df_house_purchase['price'].max()), step=1)
+
     # filter: buying_analysis
     f_only_buy = st.sidebar.checkbox('Check to see only properties suggestted to be purchased')
 
     st.sidebar.subheader('Selling Suggestions')
-    filter_column_sale = st.sidebar.multiselect('Properties Attributes', options=list(df_house_sale.columns),
+    # filter : Properties Attributes
+    f_column_sale = st.sidebar.multiselect('Properties Attributes', options=list(df_house_sale.columns),
                                                 default=['id', 'zipcode', 'price','seasonality',
                                                          'median_price_seasonality', 'condition',
                                                          'selling_price_suggestion', 'expected_profit'])
 
+    # creating main content
     st.title('House Rocket Data Report')
 
     st.subheader('Buying Suggestions')
-    purchase_analysis_ui(df_house_purchase, f_column_purchase, f_only_buy)
+    purchase_analysis_ui(df_house_purchase, f_column_purchase, f_buying_price, f_only_buy)
 
     st.subheader('Selling Suggestion')
-    sale_analysis_ui(df_house_sale, filter_column_sale)
+    st.write('Once the house is owned by the company, below we show the analysis of the '
+             'best time to sell them and what the sale price would be.')
+    sale_analysis_ui(df_house_profit, f_column_sale)
 
-    exp_profit = st.beta_expander('Click here to expand and see estimated profit table')
+    exp_profit = st.beta_expander('Click here to expand and see full analysis selling table')
 
     with exp_profit:
-        st.dataframe(df_house_profit)
+        st.dataframe(df_house_sale)
 
     return None
